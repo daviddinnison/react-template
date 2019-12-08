1013678b46e7a9956efd7b624fe39785fa47c078
daviddinnison <david.g.dinnison@gmail.com>
2019-12-08 12:30:55 -0500

    styled-components and basic core component directory

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const App = () => {
-    return <p>test</p>;
+import { MainPanel } from './core/Panels';
+
+const Wrapper = styled.div`
+    background: red;
+    height: 100vh;
+    padding: 0;
+    margin: 0;
+    width: 100vw;
+`;
+
+class App extends React.Component {
+    render() {
+        return (
+            <Wrapper>
+                <MainPanel>
+                    This is the main panel
+                </MainPanel>
+            </Wrapper>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/core/Panels.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const MainPanel = styled.div`
+    background: grey;
+    padding: 2rem;
+    text-align: center;
+`;
